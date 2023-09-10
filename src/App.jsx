96dc13028aba02f41475fbf18a62fa9b949f01e5
TaanVeer <taanveer469@gmail.com>
2023-09-10 20:26:30 +0600

    alhamdulliah props data passing and receiving done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-
-import './App.css'
-import Card from './components/cardCom/Card'
+import "./App.css";
+import Card from "./components/cardCom/Card";
 
 function App() {
-
-
-
-
+  // title data passing by props
+  let titleOne = "my title no. 1";
+  let titleTwo = "my title no. 2";
+  let titleThree = "my title no. 3";
+  // views data passing by props
+  let viewsOne = "100k";
+  let viewsTwo = "200k";
+  let viewsThree = "300k";
+  // buy now button data passing by props
+  let buyOne = "BUY NOW 1";
+  let buyTwo = "BUY NOW 2";
+  let buyThree = "BUY NOW 3";
 
   return (
     <>
-      
-      <h1 className='text-5xl text-center mb-12'>welcome to explore react card</h1>
+      <h1 className="text-5xl text-center mb-12">
+        welcome to explore react card
+      </h1>
       <div className="grid grid-cols-3 gap-3 px-12">
-
-
-        <Card></Card>
-         <Card></Card>
-         <Card></Card>
-
+        <Card title={titleOne} views={viewsOne} buy={buyOne}></Card>
+        <Card title={titleTwo} views={viewsTwo} buy={buyTwo}></Card>
+        <Card title={titleThree} views={viewsThree} buy={buyThree}></Card>
       </div>
-       
     </>
-  )
+  );
 }
 
-export default App
+export default App;

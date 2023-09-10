5bc7b98a7c6441d2581371eb0f12b26f3842e464
TaanVeer <taanveer469@gmail.com>
2023-09-10 19:33:15 +0600

    alhamdulliah card component added in a single jsx file

--- src/components/cardCom/Card.jsx
+++ src/components/cardCom/Card.jsx
@@ -1,20 +1,39 @@
 import React from "react";
-import Title from "./Title";
-import Image from "./Image";
-import Views from "./Views";
+
 
 const Card = () => {
-    return (
-        <>
-                <div className="card w-96 bg-base-100 shadow-xl">
-                    <Title></Title>
-                    <Image></Image>
-                    <Views></Views>
-                </div>
-        </>
-    )
-}
 
+    //custom css written in internal style as like js object
+    // tailwind and daisyui css written in inline style in the jsx xml elements
 
+  const cardStyle = {
+    color: "red",
+    fontSize: "24px"
+  };
+
+  const imgStyle = {
+    width: "350px",
+    height: "250px"
+  };
+  return (
+    <>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p style={cardStyle}>
+            If a dog chews shoes whose shoes does he choose?
+          </p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+        <figure style={imgStyle}>
+          <img src="./images/shoes.jpg" />
+        </figure>
+        <p className="tex-base text-center mt-2">100k</p>
+      </div>
+    </>
+  );
+};
 
 export default Card;
